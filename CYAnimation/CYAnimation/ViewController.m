ef66279f2b6158e000a94169ed9691c28950a59e
ChengYing <chengying@52yuwan.com>
2017-01-13 09:58:06 +0800

    动画效果

--- CYAnimation/CYAnimation/ViewController.m
+++ CYAnimation/CYAnimation/ViewController.m
@@ -12,6 +12,7 @@
 @interface ViewController ()
 
 @property (nonatomic, strong) MusicInOrderView *infoView;
+@property (nonatomic, strong) UIButton *testButton;
 
 @end
 
@@ -20,13 +21,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor redColor];
-    [self.view addSubview:self.infoView];
-    [self.infoView mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.centerX.equalTo(self.view.mas_centerX);
-        make.centerY.equalTo(self.view.mas_centerY);
-        make.width.equalTo(@100);
-        make.height.equalTo(@200);
-    }];
+    [self.view addSubview:self.testButton];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -36,13 +31,61 @@
     // Dispose of any resources that can be recreated.
 }
 
-#pragma mark - setters and getters 
+- (void)changeButtonFrame {
+    self.testButton.selected = !self.testButton.selected;
+    if (self.testButton.selected) {
+        UIImage *image = [UIImage imageNamed:@"room_musicBtn_bg"];
+        // 设置端盖的值
+        CGFloat top = image.size.height * 0.5;
+        CGFloat left = image.size.width * 0.5;
+        CGFloat bottom = image.size.height * 0.5;
+        CGFloat right = image.size.width * 0.5;
+        
+        // 设置端盖的值
+        UIEdgeInsets edgeInsets = UIEdgeInsetsMake(top, left, bottom, right);
+        // 设置拉伸的模式
+        UIImageResizingMode mode = UIImageResizingModeStretch;
+        
+        // 拉伸图片
+        UIImage *newImage = [image resizableImageWithCapInsets:edgeInsets resizingMode:mode];
+        
+        // 设置按钮的背景图片
+        [self.testButton setBackgroundImage:newImage forState:UIControlStateNormal];
+        
+        [UIView animateWithDuration:2.0f animations:^{
+            self.testButton.frame = CGRectMake(100, self.testButton.frame.origin.y, 2 * image.size.width, image.size.height);
+            [self.testButton layoutIfNeeded];
+        }];
+        
+    } else {
+        UIImage *image = [UIImage imageNamed:@"room_musicBtn_bg"];
+
+        [UIView animateWithDuration:2.0f animations:^{
+            self.testButton.frame = CGRectMake(100 + image.size.width, self.testButton.frame.origin.y, image.size.width, image.size.height);
+        }];
+    }
+
+}
+
+#pragma mark - setters and getters
 
 - (MusicInOrderView *)infoView {
     if (!_infoView) {
         _infoView = [[MusicInOrderView alloc] init];
     }
     return _infoView;
+}
+
+- (UIButton *)testButton {
+    if (!_testButton) {
+        _testButton = [UIButton buttonWithType:UIButtonTypeCustom];
+        UIImage *image = [UIImage imageNamed:@"room_musicBtn_bg"];
+        _testButton.frame = CGRectMake(100 + image.size.width, 300, image.size.width, image.size.height);
+        [_testButton setBackgroundImage:image forState:UIControlStateNormal];
+        [_testButton addTarget:self action:@selector(changeButtonFrame) forControlEvents:UIControlEventTouchUpInside];
+        
+    }
+    return _testButton;
 }
 
 @end
