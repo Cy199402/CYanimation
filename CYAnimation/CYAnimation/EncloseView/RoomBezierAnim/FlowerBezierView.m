8c985e9c1acc592c55a66ce7c12d6ddfd93e8db2
ChengYing <chengying@52yuwan.com>
2017-02-16 17:01:10 +0800

    动画效果基本完成

--- CYAnimation/CYAnimation/EncloseView/RoomBezierAnim/FlowerBezierView.m
+++ CYAnimation/CYAnimation/EncloseView/RoomBezierAnim/FlowerBezierView.m
@@ -14,6 +14,10 @@
 
 @interface FlowerBezierView () <UIGestureRecognizerDelegate, CAAnimationDelegate>
 
+@property (nonatomic, assign) uint32_t flowerTag;
+@property (nonatomic, strong) NSMutableArray <NSNumber *> *tags;
+@property (nonatomic, strong) NSMutableArray <NSString *> *timingFunctions;
+
 @end
 
 @implementation FlowerBezierView
@@ -22,6 +26,7 @@
 - (instancetype)init {
     if (self = [super init]) {
         self.backgroundColor = [UIColor redColor];
+        self.flowerTag = 0;
         [self addSubviews];
         [self autoLayout];
     }
@@ -40,18 +45,28 @@
 
 #pragma mark - action methods
 - (void)clickFlowerView {
-    NSLog(@"-- clickFlowerView --");
+    self.flowerTag ++;
     UIImageView *animView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"musicRoom_scatter_flower"]];
+    animView.tag = self.flowerTag;
+    [self.tags addObject:@(self.flowerTag)];
     [self addSubview:animView];
     
     CGPoint viewOrigin = animView.frame.origin;
     viewOrigin.y = viewOrigin.y + animView.frame.size.height / 2.0f;
     viewOrigin.x = viewOrigin.x + animView.frame.size.width / 2.0f;
     
+    CABasicAnimation *scaleAnimation = [CABasicAnimation animationWithKeyPath:@"transform.scale"];
+    scaleAnimation.fromValue = @(0.7);
+    scaleAnimation.toValue = @(1.5);
+    scaleAnimation.autoreverses = YES;
+    scaleAnimation.duration = 1.5;
+    
+    CABasicAnimation *alphaAnimation = [CABasicAnimation animationWithKeyPath:@"opacity"];
+    alphaAnimation.toValue = @(0);
+    alphaAnimation.beginTime = 1.0f;
+    
     CAKeyframeAnimation *pathAnimation = [CAKeyframeAnimation animationWithKeyPath:@"position"];
     pathAnimation.calculationMode = kCAAnimationLinear;
-    pathAnimation.fillMode = kCAFillModeForwards;
-    pathAnimation.removedOnCompletion = NO;
     
     CGPoint endPoint = CGPointMake(-SCREEN_WIDTH / 2 + animView.image.size.width, -SCREEN_HEIGHT / 2 + animView.image.size.height);
 
@@ -63,26 +78,36 @@
     int tempX = arc4random() % width;
     int tempY = arc4random() % height;
     
-    NSLog(@"tempX -- %d", tempX);
-    NSLog(@"tempY -- %d", tempY);
-    NSLog(@"width -- %u", width);
-    NSLog(@"height -- %u", height);
-    
     CGPoint tempPoint = CGPointMake(-tempX, -tempY);
     
-    CGPathAddCurveToPoint(curvedPath, NULL, tempPoint.x, viewOrigin.y, tempPoint.x, viewOrigin.y, tempPoint.x, tempPoint.y);
-    CGPathAddCurveToPoint(curvedPath, NULL, endPoint.x, tempPoint.y, endPoint.x, tempPoint.y, endPoint.x, endPoint.y);
+    CGPathAddCurveToPoint(curvedPath, NULL, viewOrigin.x, viewOrigin.y, tempPoint.x, tempPoint.y, endPoint.x, endPoint.y);
     pathAnimation.path = curvedPath;
     CGPathRelease(curvedPath);
     
+    int timingIndex = arc4random() % 5;
+    
+    NSLog(@"self.timingFunctions[timingIndex] -- %@", self.timingFunctions[timingIndex]);
+    NSString *timing = self.timingFunctions[timingIndex];
     CAAnimationGroup *group = [CAAnimationGroup animation];
+    group.timingFunction = [CAMediaTimingFunction functionWithName:timing];
     group.fillMode = kCAFillModeForwards;
     group.removedOnCompletion = NO;
-    [group setAnimations:[NSArray arrayWithObjects:pathAnimation, nil]];
-    group.duration = 4.0f;
+    [group setAnimations:[NSArray arrayWithObjects:alphaAnimation, scaleAnimation, pathAnimation, nil]];
+    group.duration = 2.0f;
     group.delegate = self;
     [group setValue:animView forKey:@"imageViewBeingAnimated"];
     [animView.layer addAnimation:group forKey:@"savingAnimation"];
+}
+
+- (void)animationDidStop:(CAAnimation *)anim finished:(BOOL)flag {
+    if (flag) {
+        for (UIImageView *image in [self subviews]) {
+            if (image.tag != 0 && [self.tags containsObject:@(image.tag)]) {
+                [image removeFromSuperview];
+                break;
+            }
+        }
+    }
 }
 
 #pragma mark - setters and getters
@@ -96,5 +121,23 @@
         [_flowerView addGestureRecognizer:tapGesture];
     }
     return _flowerView;
+}
+
+- (NSMutableArray <NSNumber *> *)tags {
+    if (!_tags) {
+        _tags = [[NSMutableArray alloc] init];
+    }
+    return _tags;
+}
+
+- (NSMutableArray <NSString *> *)timingFunctions {
+    if (!_timingFunctions) {
+        _timingFunctions = [[NSMutableArray alloc] initWithArray:@[kCAMediaTimingFunctionLinear,
+                                                                   kCAMediaTimingFunctionEaseIn,
+                                                                   kCAMediaTimingFunctionEaseOut,
+                                                                   kCAMediaTimingFunctionEaseInEaseOut,
+                                                                   kCAMediaTimingFunctionDefault]];
+    }
+    return _timingFunctions;
 }
 @end
